42b85ae703431450008656cc4073718005330472
esraahusamia <esraa.hus93@yahoo.com>
2017-08-26 09:39:16 -1200

    stack function

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,10 +1,18 @@
 var Stack = function() {
-    this._storage = [];
-    this.counter = 0;
-    this.add = function(value){
-      this._storage[0]=value;
-    };
-    this.remove = function() {
-    this._storage.slice(this._storage.length-1, 1)
-    };
-  };
+	
+	this._storage = [];
+	this.counter = 0;
+
+	this.add = function(value){
+
+		this._storage[0]=value;
+
+	};
+
+	this.remove = function() {
+
+		this._storage.slice(this._storage.length-1, 1);
+
+	};
+
+};
