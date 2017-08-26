6e1c4990e53180fbac2d39a82b19f305e8559586
esraahusamia <esraa.hus93@yahoo.com>
2017-08-26 09:38:16 -1200

    add extintion to array

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,18 @@
 // your code here
+
+
+function Array-Extention(array){
+
+	this.array = array
+
+	this.first(){
+
+		return this.array[0]
+	}
+
+	this.last(){
+		
+		return this.array[array.length-1]
+
+	}
+}
